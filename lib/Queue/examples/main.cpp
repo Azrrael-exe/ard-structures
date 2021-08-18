ccb20be4076aa81a803794507d86de6f12828f0b
dagarre00 <dagarre00@gmail.com>
2021-08-17 21:54:44 -0500

    Distribucion de directorios cambiada para facilitar compilacion

---
+++ lib/Queue/examples/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <queue.h>
+
+Queue queue = Queue(10);
+
+void setup(){
+    Serial.begin(115200);
+}
+
+void loop(){
+    for(int i=0; i<10; i++){
+        Serial.println("Hello World");
+        delay(500);
+    }
+}
