c6df87627348e93fc487447e48300761ec2a60b1
Daniel <dagarre00@gmail.com>
2021-08-28 10:48:03 -0500

    test map añadido a tests_native.cpp

--- test/unit/tests_native.cpp
+++ test/unit/tests_native.cpp
@@ -1,11 +1,13 @@
 #include <unity.h>
 #include "test_queue/test_queue.h"
 #include "tests_stack/test_stack.h"
+#include "test_map/test_map.h"
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_function_stack);
     RUN_TEST(test_function_queue);
+    RUN_TEST(test_function_map);
     UNITY_END();
     return 0;
 }
